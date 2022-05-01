d3834218291b2e3b12c8b606d0615f69d9722eaa
pminsung12 <52368015+pminsung12@users.noreply.github.com>
2022-05-01 23:53:30 +0900

    [Gold IV] Title: 전화번호 목록, Time: 52 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
@@ -1,42 +1,41 @@
 #include <iostream>
 #include <algorithm>
+#include <string>
 using namespace std;
-
-void checking(string num[], int n){
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            int t=min(num[i].length(),num[j].length());
-            for(int k=0;k<t;k++){
-                if(num[i][k]!=num[j][k]) break;
-                else{
-                    if(k==t-1){
-                        cout<<"NO"<<'\n';
-                        return;
-                    }
-                }
-            }
-        }
-    }
-    cout<<"YES"<<'\n';
-    return;
-}
-
-int main(){
+ 
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    int T;
+    cin >> T;
     
-    int T,n;
-    cin>>T;
-
     string num[10001];
-    while(T--){
-        cin>>n;
-        for(int i=0;i<n;i++){
-            cin>>num[i];
+    int n;
+    
+    while(T--) {
+        bool isValid = true;
+        cin >> n;
+        
+        for (int i = 0; i < n; i++) {
+            cin >> num[i];
         }
-        sort(num,num+n);
-        checking(num,n);
+ 
+        sort(num, num+n);
+ 
+        for (int k = 0; k <= n-2; k++) {
+            int current = num[k].length();
+            int next = num[k+1].length();
+ 
+            if (current >= next) continue;
+            if (num[k + 1].substr(0, current)== num[k]) {
+                isValid = false;
+                cout << "NO" << '\n';
+                break;
+            }
+        }
+ 
+        if (isValid) cout << "YES" << '\n';
     }
     return 0;
 }
