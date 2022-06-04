c42923733a9b60a3551448ee66add2708fb44c5f
pminsung12 <52368015+pminsung12@users.noreply.github.com>
2022-06-04 23:16:58 +0900

    [level 2] Title: 더 맵게, Time: 250.94 ms, Memory: 41.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int,vector<int>,greater<int>> pq;
+    for(int e:scoville)pq.push(e);
+    
+    while(pq.top()<K){
+        if(pq.size()==1) return -1;
+        int tmp1=pq.top();
+        pq.pop();
+        int tmp2=pq.top();
+        pq.pop();
+        pq.push(tmp1+tmp2*2);
+        answer++;
+    }
+    return answer;
+}
