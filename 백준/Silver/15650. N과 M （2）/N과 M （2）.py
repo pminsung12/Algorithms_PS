9dd3b92107cfe2568405c906e88c66809c9bebf6
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2023-05-01 13:54:30 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,14 @@
+import itertools
+import sys
+
+input=sys.stdin.readline
+
+N,M=list(map(int,input().split()))
+
+arr=[i for i in range(1,N+1)]
+res=list(itertools.combinations(arr,M))
+
+for i in res:
+    for j in i:
+        print(j, end=" ")
+    print()
