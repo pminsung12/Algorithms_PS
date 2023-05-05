8316e8246651bc8ba47e5536de48c65a3834306b
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2023-05-06 01:52:36 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = list(map(int, input().split()))
+arr=[i for i in range(1,N+1)]
+
+answer=[]
+num=0
+
+for i in range(N):
+    num += K-1
+    if num>=len(arr):
+        num%=len(arr)
+    
+    answer.append(str(arr.pop(num)))
+
+print("<",", ".join(answer)[:], ">", sep='')
