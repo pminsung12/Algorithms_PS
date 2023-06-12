b1f8548b87e10fb41ea4ec49e8cd2c0b45b3ea86
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2023-06-13 04:02:31 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,12 +1,6 @@
 def solution(n):
-    answer = 0
-    for i in range(1,n+1):
-        if i**2==n:
-            answer=(i+1)**2
-            break
-        if i**2>n:
-            break
-    if answer==0:
-        return -1
-    return answer
+    sqrt=n**.5
+    if sqrt%1==0:
+        return (sqrt+1)**2
+    return -1
 
