91e052322b965eb0d7d8d4bca27df353bd20210c
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2023-06-14 01:37:05 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -5,19 +5,17 @@ def solution(id_list, report, k):
     answer = [] # 메일 받는 횟수
     
     mail=defaultdict(int)
-    id=defaultdict(set) # 중복제거용 set으로 딕셔너리 사용
+    id=defaultdict(int)
 
-    for i in report:
-        x,y=i.split(' ')
-        id[y].add(x) # 신고당한 사람이 key, value set에는 날 신고한 사람 목록
+    for i in set(report):
+        id[i.split()[1]]+=1 # 신고당한 사람이 key, 신고한사람 수가 value
         
     for j in id_list: # 메일 받을 사람 딕셔너리 초기화
         mail[j]=0
 
-    for u,v in id.items():
-        if len(v)>=k: # 신고한 사람수가 2명보다 많으면
-            for w in v:
-                mail[w]+=1
+    for m in set(report):
+        if id[m.split()[1]]>=k:
+            mail[m.split()[0]]+=1
     
     for u,v in mail.items():
         answer.append(v)
