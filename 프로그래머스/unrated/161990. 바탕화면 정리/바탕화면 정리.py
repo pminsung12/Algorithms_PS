3b3b1fc2b5f5de59b44e36eee20bbc44445413fe
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2023-06-16 17:58:32 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,14 +1,10 @@
 def solution(wallpaper):
-    answer = []
+    x,y=[],[]
     
-    lux,luy,rdx,rdy=1e9,1e9,0,0
     for i in range(len(wallpaper)):
         for j in range(len(wallpaper[i])):
             if wallpaper[i][j]=='#':
-                lux=min(lux,i)
-                luy=min(luy,j)
-                rdx=max(rdx,i)
-                rdy=max(rdy,j)
+                x.append(i)
+                y.append(j)
     
-    answer=[lux,luy,rdx+1,rdy+1]
-    return answer
+    return [min(x), min(y), max(x)+1, max(y)+1]
