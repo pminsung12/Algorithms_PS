8dc0d2882fa39f21ac88cce92781678cecb2acd5
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2023-06-17 16:09:57 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,9 +1,2 @@
 def solution(A,B):
-    answer = 0
-
-    A.sort()
-    B.sort(reverse=True)
-    for i in range(len(A)):
-        answer+=A[i]*B[i]
-
-    return answer
+    return sum([i*j for i,j in zip(sorted(A), sorted(B,reverse=True))])
