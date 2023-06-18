c91a29a1c0ea8c04a132a2ebb90ced1ad517bfa8
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2023-06-19 04:08:16 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 23.59 ms, Memory: 12.8 MB -BaekjoonHub

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -2,20 +2,18 @@ def solution(cap, n, deliveries, pickups):
     deliveries=deliveries[::-1]
     pickups=pickups[::-1]
     
-    have_to_deliver=0
-    have_to_pickup=0
+    cnt_deliver=0
+    cnt_pickup=0
     
     answer=0
     for i in range(n):
-        have_to_deliver+=deliveries[i]
-        have_to_pickup+=pickups[i]
+        cnt_deliver+=deliveries[i]
+        cnt_pickup+=pickups[i]
         
-        while have_to_deliver>0 or have_to_pickup>0:
-            have_to_deliver -= cap
-            have_to_pickup -= cap
+        while cnt_deliver>0 or cnt_pickup>0:
+            cnt_deliver -= cap
+            cnt_pickup -= cap
             
             answer+= (n-i)*2
-    
-    
     
     return answer
