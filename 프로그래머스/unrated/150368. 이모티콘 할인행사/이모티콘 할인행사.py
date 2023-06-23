c5913f3def7dcda1193f3e6fe76d660010f46ae4
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2023-06-23 18:58:58 +0900

    [unrated] Title: 이모티콘 할인행사, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -0,0 +1,19 @@
+from itertools import product # 중복순열
+
+def userSpent(emoticons, ratio, user_rate):
+    return sum([emoticons[i]*(1-(ratio[i]/100)) for i in range(len(emoticons)) if ratio[i]>=user_rate])
+
+def solution(users, emoticons):
+    answer=[]
+    
+    for ratio in product([10,20,30,40], repeat=len(emoticons)):
+        emti_plus, total=0,0
+        for user in users:
+            res=userSpent(emoticons, ratio, user[0])
+            if res >=user[1]:
+                emti_plus+=1
+                continue
+            total+=res
+        answer.append([emti_plus,total])
+    
+    return sorted(answer, reverse=True)[0]
