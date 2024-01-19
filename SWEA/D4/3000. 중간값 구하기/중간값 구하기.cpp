18f2e38d46ea5cc00dbb8f7d14dc77e81b915e84
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2024-01-19 17:16:07 +0900

    [D4] Title: 중간값 구하기, Time: 4,648 ms, Memory: 32,288 KB -BaekjoonHub

--- SWEA/D4/3000. 중간값 구하기/중간값 구하기.cpp
+++ SWEA/D4/3000. 중간값 구하기/중간값 구하기.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <queue>
+#include <set>
 #include <algorithm>
 
 using namespace std;
@@ -17,39 +17,24 @@ int main() {
         ll n, m;
         cin >> n >> m;
         
-        priority_queue<ll> max_heap; // 최대 힙
-        priority_queue<ll, vector<ll>, greater<ll>> min_heap; // 최소 힙
-        max_heap.push(m);
-        min_heap.push(m);
+        multiset<ll> ms;
+        ms.insert(m);
+        
+        multiset<ll>::iterator it = ms.begin();
         
         ll ans=0;
         for (ll i = 1; i <= n; i++) {
             ll x, y;
             cin >> x >> y;
+            ms.insert(x);
+            ms.insert(y);
             
-            if(max_heap.top() > x) 
-                max_heap.push(x);
-            else
-                min_heap.push(x);
+            int mid = *it;
             
-            if(max_heap.top() > y)
-                max_heap.push(y);
-            else
-                min_heap.push(y);
-                
-            if(max_heap.size() == min_heap.size()){
-                ans+=(max_heap.top() % 20171109);
-                continue;
-            }
-            else if(max_heap.size() > min_heap.size()){
-                max_heap.pop();
-                min_heap.push(max_heap.top());
-            }
-            else{
-                min_heap.pop();
-                max_heap.push(min_heap.top());
-            }
-            ans += (max_heap.top() % 20171109);
+            if(x>=mid && y>=mid) it++;
+            else if(x<=mid && y<=mid) it--;
+            
+            ans += *it % 20171109;
         }
         
         cout << '#' << t << ' ' << ans % 20171109 << endl;
