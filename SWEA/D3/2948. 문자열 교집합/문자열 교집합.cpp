a48c08583582c9918f606659166ed48fb04a8bf2
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2024-01-24 23:44:55 +0900

    [D3] Title: 문자열 교집합, Time: 1,316 ms, Memory: 24,744 KB -BaekjoonHub

--- SWEA/D3/2948. 문자열 교집합/문자열 교집합.cpp
+++ SWEA/D3/2948. 문자열 교집합/문자열 교집합.cpp
@@ -5,29 +5,29 @@
 #include <algorithm>
 #define MAX 51
 using namespace std;
-
+ 
 int n, m;
 unordered_set<string> words;
-
+ 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
+     
     int T;
     cin >> T;
-
+ 
     for (int testCase = 1; testCase <= T; testCase++) {
         cin >> n >> m;
-        
+         
         int cnt=0;
         string word;
-        
+         
         for(int i=0; i<n; i++){
             cin >> word;
             words.emplace(word);
         }
-        
+         
         for(int j=0; j<m; j++){
             cin >> word;
             if(words.find(word)!=words.end()) 
@@ -36,6 +36,6 @@ int main() {
         cout << '#' << testCase << ' ' << cnt << endl;
         words.clear();
     }
-
+ 
     return 0;
 }
