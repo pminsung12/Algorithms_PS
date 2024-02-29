070b404ecf864ecf8d60fd9f6379c20880221de3
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2024-02-29 16:10:15 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 216 ms, Memory: 37044 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,37 @@
+import heapq
+
+INF=int(1e9)
+ans=INF
+x,y=map(int,input().split())
+distance=[INF]*100001
+
+def dijkstra(start):
+    q=[]
+    heapq.heappush(q,(0,start))
+    distance[start]=0
+    while q:
+        dist, n=heapq.heappop(q)
+        
+        if distance[n] < dist :
+            continue
+        
+        # X-1
+        cost=distance[n]+1
+        if n-1>=0 and cost < distance[n-1]:
+            distance[n-1]=cost
+            heapq.heappush(q,(cost,n-1))
+        
+        # X+1
+        cost=distance[n]+1
+        if n+1<=y and cost < distance[n+1]:
+            distance[n+1]=cost
+            heapq.heappush(q,(cost,n+1))
+        
+        # X*2
+        cost=distance[n]
+        if  n*2<=y*2 and n*2<=100000 and cost < distance[2*n]:
+            distance[n*2]=cost
+            heapq.heappush(q,(cost,n*2))
+        
+dijkstra(x)
+print(distance[y])
