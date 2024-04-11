8b4027124ad12f9b9f52033597963c0b89f1f205
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2024-04-11 15:55:57 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 4.42 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,26 +1,17 @@
 import java.util.*;
 
 class Solution {
-    public List<int[]> solution(int[][] data, String ext, int val_ext, String sort_by) {
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
 
-        List<int[]> list = new ArrayList<>();
-        String[] s = {"code","date","maximum","remain"};
-        int extIdx=0, sortIdx=0;
+        Map<String, Integer> idxMap = new HashMap<>();
+        idxMap.put("code",0);
+        idxMap.put("date",1);
+        idxMap.put("maximum",2);
+        idxMap.put("remain",3);
+        
+        int[][] arr = Arrays.stream(data).filter(x->x[idxMap.get(ext)]<val_ext).toArray(int[][]::new);
+        Arrays.sort(arr, (a,b)->a[idxMap.get(sort_by)]-b[idxMap.get(sort_by)]);
 
-        for(int i=0; i<4; i++){
-            if(ext.equals(s[i])) extIdx=i;
-            if(sort_by.equals(s[i])) sortIdx=i;
-        }
-        final int si=sortIdx;
-
-        for(int i=0; i<data.length; i++){
-            if(data[i][extIdx]<val_ext) list.add(data[i]);
-        }
-
-        // *lambda, Comparator 사용시 final 이나 effectively final여야한다!
-        Collections.sort(list,(o1,o2)->o1[si]-o2[si]);
-
-
-        return list;
+        return arr;
     }
 }
