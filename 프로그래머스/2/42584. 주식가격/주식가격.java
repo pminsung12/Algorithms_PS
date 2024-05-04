15088e5e9cce832bde09760d71c278e14c598bf0
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2024-05-04 15:33:55 +0900

    [level 2] Title: 주식가격, Time: 12.81 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,15 +1,19 @@
-class Solution {
-	public int[] solution(int[] prices) {
-		int[] answer = new int[prices.length];
+import java.util.*;
 
-		for (int i = 0; i < prices.length; i++) {
-			for (int j = i + 1; j < prices.length; j++) {
-				answer[i]++; // 뒤에 있는 값들 보다 작거나 같을 때 인덱스 값 하나씩 추가
-				if (prices[i] > prices[j]) { // 크면 다음 인데스 비교
-					break;
-				}
-			}
-		}
-		return answer;
-	}
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        for(int i=0; i<prices.length-1; i++){
+            for(int j=i+1; j<prices.length; j++){
+                // 일단 1초 뒤에 가격이 변동되기 때문에 +1은 무조건
+                answer[i]+=1;
+                if(prices[i]>prices[j]){ // 가격이 떨어졌을 때
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
 }
