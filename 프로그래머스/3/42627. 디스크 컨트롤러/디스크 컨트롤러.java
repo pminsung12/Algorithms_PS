065817e8b5f65394f4c125256bcf48ec1d0dd1fd
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2024-05-05 00:54:40 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.03 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -7,14 +7,17 @@ class Solution {
         
         int now=0, answer=0, jobIdx=0, cnt=0;
         
-        while(cnt<jobs.length){
+        while(cnt<jobs.length){ // 모든 jobs 처리할 때까지
+            // 지금 시점에서 들어온 job들만 힙에 넣기
             while(jobIdx<jobs.length && jobs[jobIdx][0] <= now){
                 pq.offer(jobs[jobIdx++]);
             }
             
+            // 큐가 비어있으면 아직 요청을 기다리는 거니까 다음 요청 시작점으로 슉 옮기기
             if(pq.isEmpty()){
                 now = jobs[jobIdx][0];
             } else{
+                // 힙에 넣었으니 가장 수행시간이 짧은 요청부터 끝내기!
                 int[] job=pq.poll();
                 answer+= now+job[1]-job[0];
                 now+=job[1];
@@ -23,7 +26,6 @@ class Solution {
              
         }
         
-        int res = answer/jobs.length;
         return (int) answer/jobs.length ;
     }
 }
