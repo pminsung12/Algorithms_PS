c9830efcd62eccab718518a58b7f92f7ed636a46
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2024-09-29 00:42:59 +0900

    [Gold V] Title: 암호 만들기, Time: 112 ms, Memory: 15628 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,69 +1,66 @@
 import java.util.*;
+import java.io.*;
 
-public class Main {
-	static int L, C;
-	static char[] data;
-	static List<String> result;
-	static StringBuilder sb;
+class Main {
+  
+  static int L, C;
+  static char[] inputs;
+  static boolean[] visited;
+  static StringBuilder sb;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-		L = sc.nextInt();
-		C = sc.nextInt();
+    L = Integer.parseInt(st.nextToken());
+    C = Integer.parseInt(st.nextToken());
 
-		data = new char[C];
-		sb= new StringBuilder();
-		result = new ArrayList<>();
+    inputs = new char[C];
+    visited = new boolean[C];
+    sb = new StringBuilder();
 
-		for (int i = 0; i < C; i++) {
-			data[i] = sc.next().charAt(0);
-		}
+    st = new StringTokenizer(br.readLine());
+    for(int c=0; c<C; c++){
+      inputs[c]=st.nextToken().charAt(0);
+    }
+    
+    Arrays.sort(inputs);
 
-		Arrays.sort(data);
+    for(int c=0; c<C; c++){
+      if(isVowel(inputs[c])){
+        dfs(c, 0, 1);
+      } else{
+        dfs(c, 1, 0);
+      }
+    }
+  }
 
-		dfs(-1, 0, 0);
+  static boolean isVowel(char c){
+    if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+      return true;
+    }
+    return false;
+  }
 
-		for (String s : result) {
-			System.out.println(s);
-		}
-
-	}
-
-	static void dfs(int current, int con, int vow) {
-		// 1. 체크인: 현재까지 만들어진 암호
-		if (current >= 0) {
-			sb.append(data[current]);
-		}
-
-		// 2. 목적지인가: length == L, 자음, 모음 조건 -> 암호출력
-		if (sb.length() == L) {
-			if (con >= 2 && vow >= 1) {
-				result.add(sb.toString());
-			}
-		} else {
-			// 3. 연결된 곳을 순회: current+1 ~ C
-			for (int i = current + 1; i < C; i++) {
-				// 4. 갈 수 있는가?(생략-> 목적지에서 정의)
-				// 5. 간다: 자음, 모음
-				if (isVow(data[i]))
-					dfs(i, con, vow + 1);
-				else
-					dfs(i, con + 1, vow);
-			}
-
-		}
-
-		// 6. 체크아웃: 현재까지 만들어진 암호
-		if (current >= 0) {
-			sb.deleteCharAt(sb.length() - 1);
-		}
-	}
-
-	static boolean isVow(char c) {
-		if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
-			return true;
-		return false;
-	}
-
+  static void dfs(int cur, int con, int vow){
+    // 1. 체크인: 현재까지 만들어진 암호
+    sb.append(inputs[cur]);
+    // 2. 목적지인가: length==L, 자음, 모음 조건건 -> 암호 출력
+    if(sb.length()==L && con>=2 && vow>=1){
+      System.out.println(sb.toString());
+    } else{
+      // 3. 연결된 곳을 순회
+      for(int next=cur+1; next<C; next++){
+        // 4. 갈 수 있다면
+        if(isVowel(inputs[next])){
+          // 방문
+          dfs(next, con, vow+1);
+        } else {
+          dfs(next, con+1, vow);
+        }
+      }
+    }
+    sb.deleteCharAt(sb.length()-1);
+  }
 }
+    
