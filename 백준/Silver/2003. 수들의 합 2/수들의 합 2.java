895175e12a8a6482fc8ceee20a024415aeb36165
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2024-10-08 18:39:07 +0900

    [Silver IV] Title: 수들의 합 2, Time: 140 ms, Memory: 15464 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,41 +1,43 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-	
-	static int N,M, sum, low, high, cnt=0;
-	static int[] input;
+class Main{
+  
+  static int N, M;
+  static int[] nums;
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		input= new int[N+1];
-		
-		for(int i=0; i<N; i++) {
-			input[i] = Integer.parseInt(st.nextToken());
-		}
-		input[N]=0;
-		sum=input[0]; low=0; high=0;
-		while(high<=N-1 && low<=N-1) {
-			if(sum < M) {
-				high++;
-				sum+=input[high];
-			} else if (sum == M) {
-				cnt++;
-				high++;
-				sum+=input[high];
-			} else {
-				sum-=input[low];
-				low++;
-			}
-		}
-		
-		System.out.println(cnt);
-		
-	}
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ 
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    
+    nums = new int[N];
+    ArrayDeque<Integer> q = new ArrayDeque<>();
 
+    int sum=0, res=0;
+    st = new StringTokenizer(br.readLine());
+    for(int n=0; n<N; n++){      
+      int cur = Integer.parseInt(st.nextToken());
+      q.add(cur);
+      sum+=cur;
+
+      while(sum>M){
+        if(!q.isEmpty()){
+          sum-=q.poll();
+        }
+      }
+      
+      if(sum==M){
+        res++;
+      }
+    }
+    
+    bw.write(String.valueOf(res));    
+    bw.flush();
+    bw.close();
+    br.close();
+  }
 }
