15a398ea8de6c0f705da1d3e552598e174213c3f
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2024-10-09 16:59:32 +0900

    [Gold IV] Title: 부분합, Time: 268 ms, Memory: 24376 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,43 +1,51 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
+class Main{
+  
+  static int N, M;
+  static int[] input;
 
-	static int N, M, sum, low, high, res = 100001;
-	static int[] input;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ 
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+    input = new int[N+1];
+    
+    st = new StringTokenizer(br.readLine());
+    for(int n=0; n<N; n++){
+        input[n] = Integer.parseInt(st.nextToken());
+    }
 
-		st = new StringTokenizer(br.readLine());
-		input = new int[N + 1];
+    // System.out.println(Arrays.toString(input));
 
-		for (int i = 0; i < N; i++) {
-			input[i] = Integer.parseInt(st.nextToken());
-		}
-		input[N] = 0;
-		sum = input[0];
-		low = 0;
-		high = 0;
-		while (high <= N - 1 && low <= N - 1) {
-			if (sum < M) {
-				high++;
-				sum += input[high];
-			} else if (sum >= M) {
-				res = Math.min(res, high - low + 1);
-				sum -= input[low];
-				low++;
-			}
-		}
-		if (res == 100001) {
-			System.out.println(0);
-			return;
-		}
-		System.out.println(res);
+    int low=0, high=0, sum=input[0], minLength=100001;
+    while(true){
+      if(sum >= M){
+        minLength = Math.min(minLength, high-low+1);
+        sum-=input[low];
+        low++;
+      } else {
+        high++;
+        sum+=input[high];
+      }
 
-	}
+      if(high==N){
+        break;
+      }
+    }
+    if(minLength == 100001){
+      bw.write("0");
+    } else {
+      bw.write(String.valueOf(minLength));
+    }
+    bw.flush();
+    bw.close();
+    br.close();
 
+  }
 }
