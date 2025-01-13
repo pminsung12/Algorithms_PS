647fc870447237bd8b77f2f687a0a1bdd368960b
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2025-01-13 18:59:34 +0900

    [level 2] Title: 롤케이크 자르기, Time: 285.26 ms, Memory: 197 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,32 +1,28 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] topping) {
+    public int solution(int[] toppings) {
         int answer = 0;
         Map<Integer, Integer> oMap = new HashMap<>();
         Map<Integer, Integer> yMap = new HashMap<>();
         
-        for(int t: topping){
-            oMap.put(t, oMap.getOrDefault(t,0)+1);
+        for(int topping: toppings){
+            oMap.put(topping, oMap.getOrDefault(topping, 0) +1);
         }
         
-        for(int t: topping){
-            yMap.put(t, yMap.getOrDefault(t,0)+1);
-            
-            if(oMap.get(t)-1==0){
-                oMap.remove(t);
+        for(int topping: toppings){
+            yMap.put(topping, yMap.getOrDefault(topping, 0) +1);
+            if(oMap.get(topping)-1==0){
+                oMap.remove(topping);
             } else {
-                oMap.put(t, oMap.get(t)-1);
+                oMap.put(topping, oMap.getOrDefault(topping,0)-1);
             }
             
             if(yMap.size()==oMap.size()){
                 answer++;
             }
+            
         }
-        
-        
-        
-        
         
         return answer;
     }
