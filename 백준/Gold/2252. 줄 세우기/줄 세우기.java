fd986b56a1dfd5401d25b877c5dcf44a13c120dc
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2025-01-16 02:41:03 +0900

    [Gold III] Title: 줄 세우기, Time: 444 ms, Memory: 45332 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,66 +1,57 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
-  
-  static int N, M, from, to;
+
+  static int N,M;
   static int[] indegree;
-  static StringBuilder sb;
   static List<Integer>[] adjList;
 
-  public static void main(String[] args) throws IOException{
+  public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
-    sb = new StringBuilder();
 
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
-
     adjList = new ArrayList[N+1];
     indegree = new int[N+1];
-
-    for(int n=1; n<=N; n++){
-      adjList[n]=new ArrayList<>();
+    for(int i=0; i<=N; i++){
+      adjList[i] = new ArrayList<>();
     }
 
-    for(int m=0; m<M; m++){
+    int from, to;
+    for(int m =0; m<M; m++){
       st = new StringTokenizer(br.readLine());
       from = Integer.parseInt(st.nextToken());
       to = Integer.parseInt(st.nextToken());
-      indegree[to]++;
       adjList[from].add(to);
+      indegree[to]++;
     }
-    
-    topologySort();
-    bw.write(sb.toString());
-    bw.flush();
-    bw.close();
-    br.close();
+    String s = topologySort();
+    System.out.println(s);
   }
 
-  static void topologySort(){
+  static String topologySort(){
+    StringBuilder sb = new StringBuilder();
     ArrayDeque<Integer> q = new ArrayDeque<>();
-
-    // 시작점:차수가 0인 노드 queue에 넣기
-    for(int n=1; n<=N; n++){
-      if(indegree[n]==0){
-        q.offer(n);
+    for(int i=1; i<=N ;i++){
+      if(indegree[i]==0){
+        q.add(i);
       }
     }
 
     while(!q.isEmpty()){
-      int cur = q.poll();
-      sb.append(cur+" ");
-
-      for(int next: adjList[cur]){
-        // cur -> next 차수 줄이기
-        indegree[next]--;
-        // 다음 시작점:차수가 0인 노드 queue에 넣기
-        if(indegree[next]==0){
-          q.offer(next);
+      int from = q.poll();
+      sb.append(from).append(' ');
+      for(int to: adjList[from]){
+        indegree[to]--;
+        if(indegree[to]==0){
+          q.add(to);
         }
       }
     }
+
+    return sb.toString();
   }
+
 }
