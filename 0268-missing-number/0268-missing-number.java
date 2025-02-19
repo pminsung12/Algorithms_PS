905b666473087beba67d6c7adf9d851b52e079d0
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2025-02-20 01:56:17 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i]+1 == nums[i+1]){
+                continue;
+            }
+            return i+1;
+        }
+        return nums.length;
+    }
+}
