1e449d981490ba15e47d15a85d2ad38885299ef2
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2025-02-19 23:35:22 +0900

    Time: 10 ms (88.41%), Space: 58.3 MB (41.88%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int i: nums){
+            if(set.contains(i)){
+                return true;
+            }
+            set.add(i);
+        }
+        
+        return false;
+        
+    }
+}
