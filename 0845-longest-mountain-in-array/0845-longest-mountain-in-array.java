41038d6ac0cef30a346be4bde72c7f742eb84e0e
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2025-02-26 03:23:27 +0900

    Time: 3 ms (61.96%), Space: 45.1 MB (51.05%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.java
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.java
@@ -1,6 +1,8 @@
 class Solution {
     public int longestMountain(int[] arr) {
-
+        if(arr.length<3){
+            return 0;
+        }
         int maxLen = 0;
         int len = 1;
         boolean down = false;
@@ -17,6 +19,10 @@ class Solution {
                     len+=1;
                     maxLen = Math.max(maxLen, len);
                 }
+            } else{
+                // 같을 때
+                down = false;
+                len=1;
             }
         }
 
