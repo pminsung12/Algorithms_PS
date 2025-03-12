b79f9a8110334609f06eb958e9188bbfe781a3b4
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2025-03-12 16:25:41 +0900

    Time: 1 ms (48.96%), Space: 18 MB (32.41%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,5 +2,16 @@ from itertools import permutations
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(permutations(nums, len(nums)))
+        answer = []
+        def backtrack(start, end):
+            if(start == end):
+                answer.append(nums[:])
+                return
+            for i in range(start, end):
+                nums[start], nums[i] = nums[i], nums[start]  # 스왑
+                backtrack(start + 1, end)  # 다음 인덱스로 진행
+                nums[start], nums[i] = nums[i], nums[start]  # 원상복구 (백트래킹)
+        backtrack(0,len(nums))
+        return answer
+        # return list(permutations(nums, len(nums)))
         
