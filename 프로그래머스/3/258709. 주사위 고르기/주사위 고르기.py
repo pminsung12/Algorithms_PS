e7ae992842888811db85072fc315024b946ec5bd
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2025-03-21 15:58:23 +0900

    [level 3] Title: 주사위 고르기, Time: 4996.48 ms, Memory: 9.66 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -2,19 +2,20 @@ from itertools import combinations, product
 from bisect import bisect_left
 
 def solution(dices):
-    dic = {}
+    dic={}
     L = len(dices)
-    for A_index_combi in combinations(range(L), L//2):
-        B_index_combi = [i for i in range(L) if i not in A_index_combi]
+    for A_index_comb in combinations(range(L), L//2):
+        B_index_comb = [i for i in range(L) if i not in A_index_comb]
         
-        A,B = [], []
-        for order_product in product(range(6), repeat=L//2):
-            A.append(sum(dices[i][j] for i, j in zip(A_index_combi, order_product)))
-            B.append(sum(dices[i][j] for i, j in zip(B_index_combi, order_product)))
-    
+        A=[]
+        B=[]
+        for dice_idx in product(range(6), repeat=L//2):
+            A.append(sum(dices[i][j] for i, j in zip(A_index_comb, dice_idx)))
+            B.append(sum(dices[i][j] for i, j in zip(B_index_comb, dice_idx)))
+        
         B.sort()
         wins = sum(bisect_left(B, num) for num in A)
-        dic[wins]=list(A_index_combi)
+        dic[wins] = A_index_comb
     
-    max_key = max(dic.keys())
-    return [x+1 for x in dic[max_key]]
+    max_win = max(dic.keys())
+    return [i+1 for i in dic[max_win]]
