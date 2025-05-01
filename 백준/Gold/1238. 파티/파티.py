a28d839e5dd7ac9173b2d847510d0caf65daeac0
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2025-05-01 17:38:01 +0900

    [Gold III] Title: 파티, Time: 1848 ms, Memory: 35508 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.py
@@ -0,0 +1,41 @@
+import heapq
+
+def dijkstra(start):
+  pq = []
+  distance = [1e9 for _ in range(n+1)]
+  distance[start]=0
+  heapq.heappush(pq, (0, start))
+  
+  while pq:
+    dist, now = heapq.heappop(pq)
+    if distance[now] < dist:
+      continue
+    
+    for next, c in adjList[now]:
+      cost = dist + c
+      if cost<distance[next]:
+        distance[next] = cost
+        heapq.heappush(pq, (cost, next))
+  return distance
+
+n, m ,x = map(int, input().split())
+
+# 단방향임
+# 각각의 학생 집에서 x까지 거리 + x에서 집까지 다익스트라 돌리고
+# 이것들의 최댓값
+
+adjList = [[] for _ in range(n+1)]
+for i in range(m):
+  u, v, dist = map(int, input().split())
+  adjList[u].append((v,dist))
+  
+res = []
+max_dist=0
+for i in range(1, n+1):
+  go = dijkstra(i)
+  come = dijkstra(x)
+  max_dist = max(max_dist, go[x]+come[i])
+  
+print(max_dist)
+
+
